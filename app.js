f950c0ef7a22a113947cffa506321061c227a441
Ashlyn Knox <ashx3s@tuta.io>
2026-01-22 17:42:47 -0700

    middleware for css and html on routes

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
 "use strict";
-
+// path helps us work with file locations
+const path = require("path");
 const express = require("express");
 const app = express();
 
 const port = 3000;
+const pages = path.join(__dirname, "pages");
 
-app.get("/", (request, response) => {
-  response.send("Hello World!");
+// app.use is for middleware
+app.use(express.static("public"));
+
+// app.get serves a route on a get request
+app.get("/", (req, res) => {
+  res.sendFile(path.join(pages, "index.html"));
 });
 
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(pages, "about.html"));
+});
+
+app.get("/hello", (req, res) => {
+  res.send("Hello World!");
+});
+
+// app.listen makes express listen for activity on a specific port
 app.listen(port, () => {
   console.log(`Example app listening on http://localhost:${port}`);
 });
